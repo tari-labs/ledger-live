038bfde2ad5dd80c749e9c50547dcc77dd4bf215
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-05-28 16:44:15 +0100

    Get public alpha key APDU support
    Initial implementation, will likely evolve as we define custom
    derivations.

--- libs/ledgerjs/packages/hw-app-minotari/src/MinoTari.ts
+++ libs/ledgerjs/packages/hw-app-minotari/src/MinoTari.ts
@@ -20,4 +20,18 @@ export default class MinoTari {
       version: response.subarray(0, response.length - 2).toString("ascii"),
     };
   }
+
+  async getPublicAlphaKey(account: bigint) {
+    // Buffer length plus account number as u64
+    const buffer = Buffer.alloc(9);
+    buffer[0] = 8; // 64 bits fixed
+    buffer.writeBigUInt64LE(account, 1);
+    const response = await this.transport.send(CLA, 0x03, 0, 0, buffer);
+    const version = response[0];
+    if (version != 1) {
+      throw new Error(`Unsupported response version received: ${version}`);
+    }
+    const key = response.subarray(1, response.length - 2).toString("hex");
+    return key;
+  }
 }
--- libs/ledgerjs/packages/hw-app-minotari/tests/MinoTari.unit.test.ts
+++ libs/ledgerjs/packages/hw-app-minotari/tests/MinoTari.unit.test.ts
@@ -16,5 +16,18 @@ describe("MinoTari app", () => {
         version: "1.0.0-pre.13",
       });
     });
+    it("retrieves public alpha from app", async () => {
+      const transport = await openTransportReplayer(
+        RecordStore.fromString(`
+                => 8003000009083c00000000000000
+                <= 01d0cbe7457c4a0b23b487de832351bb66c5c4fbcd08121842b7ab36577d31822c9000
+                `),
+      );
+      const app = new MinoTari(transport);
+      const publicAlphaKey = await app.getPublicAlphaKey(BigInt(60));
+      expect(publicAlphaKey).toStrictEqual(
+        "d0cbe7457c4a0b23b487de832351bb66c5c4fbcd08121842b7ab36577d31822c",
+      );
+    });
   });
 });
