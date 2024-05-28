fb8e9f82f8b5f0bf9e544d45943b4c0373a0c9ea
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-05-28 17:01:15 +0100

    APDU interface for get public key
    Gets public key from device.
    Likely to be altered as the implementation progresses
    STRIKE-2169

--- libs/ledgerjs/packages/hw-app-minotari/src/MinoTari.ts
+++ libs/ledgerjs/packages/hw-app-minotari/src/MinoTari.ts
@@ -21,12 +21,46 @@ export default class MinoTari {
     };
   }
 
+  /**
+   *
+   * @param account Account type
+   * @returns public alpha key received from device
+   */
   async getPublicAlphaKey(account: bigint) {
     // Buffer length plus account number as u64
     const buffer = Buffer.alloc(9);
     buffer[0] = 8; // 64 bits fixed
     buffer.writeBigUInt64LE(account, 1);
     const response = await this.transport.send(CLA, 0x03, 0, 0, buffer);
+    const version = response[0];
+    if (version != 1) {
+      throw new Error(`Unsupported response version received: ${version}`);
+    }
+    const key = response.subarray(1, response.length - 2).toString("hex");
+    return key;
+  }
+
+  /**
+   *
+   * @param account Account number
+   * @param keyIndex zero based index
+   * @param keyType between 1 and 4
+   *
+   * Key Types:
+   * Alpha - 1
+   * Nonce - 2
+   * Recovery - 3
+   * SenderOffset - 4
+   * @returns public key returned from device
+   */
+  async getPublicKey(account: bigint, keyIndex: bigint, keyType: bigint) {
+    // buffer length plus 3 uint64s
+    const buffer = Buffer.alloc(1 + 3 * 8);
+    buffer[0] = 3 * 8;
+    buffer.writeBigUInt64LE(account, 1);
+    buffer.writeBigUInt64LE(keyIndex, 9);
+    buffer.writeBigUInt64LE(keyType, 17);
+    const response = await this.transport.send(CLA, 0x04, 0, 0, buffer);
     const version = response[0];
     if (version != 1) {
       throw new Error(`Unsupported response version received: ${version}`);
--- libs/ledgerjs/packages/hw-app-minotari/tests/MinoTari.unit.test.ts
+++ libs/ledgerjs/packages/hw-app-minotari/tests/MinoTari.unit.test.ts
@@ -29,5 +29,19 @@ describe("MinoTari app", () => {
         "d0cbe7457c4a0b23b487de832351bb66c5c4fbcd08121842b7ab36577d31822c",
       );
     });
+
+    it("retrieves public key from app", async () => {
+      const transport = await openTransportReplayer(
+        RecordStore.fromString(`
+                => 8004000019183c0000000000000000000000000000000100000000000000
+                <= 0116c7fc82ee83daa558182257137f0dd4eb781caffde1e232f86c4d9c0085ca4c9000
+                `),
+      );
+      const app = new MinoTari(transport);
+      const publicKey = await app.getPublicKey(BigInt(60), BigInt(0), BigInt(1));
+      expect(publicKey).toStrictEqual(
+        "16c7fc82ee83daa558182257137f0dd4eb781caffde1e232f86c4d9c0085ca4c",
+      );
+    });
   });
 });
